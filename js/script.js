bc1f57a21ed2b34d34d1d8643cd99c899259abfa
sophiemadleine <sophie.vollmer@aol.de>
2023-06-22 11:25:37 +0200

    start over 2

---
+++ js/script.js
@@ -0,0 +1,47 @@
+
+function onS(letters) {
+    letters.setAttribute('src', 'img/hero/letterImg/S.png');
+  }
+  function offS(letters) {
+    letters.setAttribute('src', 'img/hero/letterBlack/S.png');
+  }
+  
+  
+  function onO(letters) {
+    letters.setAttribute('src', 'img/hero/letterImg/O.png');
+  }
+  function offO(letters) {
+    letters.setAttribute('src', 'img/hero/letterBlack/O.png');
+  }
+  
+  
+  function onP(letters) {
+    letters.setAttribute('src', 'img/hero/letterImg/P.png');
+  }
+  function offP(letters) {
+    letters.setAttribute('src', 'img/hero/letterBlack/P.png');
+  }
+  
+  
+  function onH(letters) {
+    letters.setAttribute('src', 'img/hero/letterImg/H.png');
+  }
+  function offH(letters) {
+    letters.setAttribute('src', 'img/hero/letterBlack/H.png');
+  }
+  
+  
+  function onI(letters) {
+    letters.setAttribute('src', 'img/hero/letterImg/I.png');
+  }
+  function offI(letters) {
+    letters.setAttribute('src', 'img/hero/letterBlack/I.png');
+  }
+  
+  
+  function onE(letters) {
+    letters.setAttribute('src', 'img/hero/letterImg/E.png');
+  }
+  function offE(letters) {
+    letters.setAttribute('src', 'img/hero/letterBlack/E.png');
+  }
